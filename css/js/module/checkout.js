2627a5002bb6b9a417590f996730eaebbd5d5ebc
yerlinstic <163851631+yerlinstic@users.noreply.github.com>
2024-04-24 07:40:42 -0500

    modificacion

---
+++ css/js/module/checkout.js
@@ -0,0 +1,7 @@
+git config --global user.name "yerlinstic"
+git config --global user.email "163851631+yerlinstic@users.noreply.github.com."
+
+# Configurar el editor predeterminado que Git utilizará al abrir mensajes de confirmación (commits) o al realizar otras 
+#operaciones que requieren la edición de texto, en este ejemplo "visual studio code"
+
+git config --global core.editor "code --wait"
